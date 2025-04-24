c757446f7a0ec5c56c9a84a716113a878dbcefac
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2025-04-24 17:05:36 +0530

    fix: proto defs

--- wal.go
+++ wal.go
@@ -3,7 +3,9 @@ package walgo
 import (
 	"bufio"
 	"context"
+	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"sync"
@@ -79,8 +81,80 @@ func OpenWAL(directory string, enableFsync bool, maxFileSize int64, maxSegments 
 		ctx: ctx,
 		cancel: cancel,
 	}
+
+	if wal.lastSequenceNo,err=wal.getLastSequenceNo();err!=nil{
+		return nil,err
+	}
+
+
     
 	return wal,nil
 	
 }
 
+func (wal *WAL) getLastSequenceNo() (uint64,error){
+	entry, err := wal.getLastEntryInLog()
+	if err != nil {
+		return 0, err
+	}
+
+	if entry != nil {
+		return entry.GetLogSequenceNumber(), nil
+	}
+	return 0, nil
+}
+
+func (wal *WAL) getLastEntryInLog() (*WAL_Entry, error) {
+	file, err := os.OpenFile(wal.currentSegment.Name(), os.O_RDONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var previousSize int32
+	var offset int64
+	var entry *WAL_Entry
+
+	for {
+		var size int32
+		if err := binary.Read(file, binary.LittleEndian, &size); err != nil {
+			if err == io.EOF {
+				// End of file reached, read the last entry at the saved offset.
+				if offset == 0 {
+					return entry, nil
+				}
+
+				if _, err := file.Seek(offset, io.SeekStart); err != nil {
+					return nil, err
+				}
+
+				// Read the entry data.
+				data := make([]byte, previousSize)
+				if _, err := io.ReadFull(file, data); err != nil {
+					return nil, err
+				}
+
+				entry, err = unmarshalAndVerifyEntry(data)
+				if err != nil {
+					return nil, err
+				}
+
+				return entry, nil
+			}
+			return nil, err
+		}
+
+		// Get current offset
+		offset, err = file.Seek(0, io.SeekCurrent)
+		previousSize = size
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Skip to the next entry.
+		if _, err := file.Seek(int64(size), io.SeekCurrent); err != nil {
+			return nil, err
+		}
+	}
+}
